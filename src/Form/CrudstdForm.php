5453fe3d2d1cf6fcc03ed00cf26c9b831ddd43ae
Gregory Jarrige <gregory_jarrige@yahoo.fr>
2016-09-18 15:14:15 +0200

    création classe CrudstdForm

--- src/Form/CrudstdForm.php
+++ src/Form/CrudstdForm.php
@@ -17,15 +17,17 @@ use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 /**
- * Description of AlbumbdForm
+ * Description of CrudstdForm
  *
- * @author grego
+ * @author Gregory Jarrige
+ * @version 0.1 (2016-09-18)
  */
-class AlbumbdForm {
+class CrudstdForm {
 
-    private $form ;
-    private $crud_id;
-    private $crud_context;
+    protected $form ;
+    protected $crud_id;
+    protected $crud_context;
+    protected $default_attrs;
     
     public function __construct($app, $request) {
 
@@ -59,29 +61,12 @@ class AlbumbdForm {
         }
 
         // Classe CSS par défaut (Bootstrap)
-        $default_attrs = array('class'=>'form-control');
+        $this->default_attrs = array('class'=>'form-control');
         
-        // Ajout d’attributs HTML complémentaires selon le contexte
-        switch ($this->crud_context) {
-            case 'C': {
-                break;
-            }
-            case 'R': {
-                $default_attrs['disabled'] = 'disabled';
-                break;
-            }
-            case 'U': {
-                break;
-            }
-            case 'D': {
-                $default_attrs['disabled'] = 'disabled';
-                break;
-            }
-            default: {
-                throw new \Exception('Le parametre crud_context est incorrect') ;
-            }
-        }
-    
+        // Définition des attributs par défaut
+        $this->setAttributes();
+        
+        // Définition des champs cachés "crud" et "id"
         if ($this->crud_context == 'U' || $this->crud_context == 'C') {
             $this->form        
             ->add('crud', HiddenType::class, array(
@@ -89,41 +74,6 @@ class AlbumbdForm {
             ))
             ->add('id', HiddenType::class, array(
                 'constraints' => array(new Assert\NotBlank())
-            ))        
-            ->add('album', TextType::class, array(
-                'constraints' => array(new Assert\NotBlank(), 
-                    new Assert\Length(array('min' => 2)),
-                    new Assert\Length(array('max' => 30))
-                ),
-                'attr' => $default_attrs       
-            ))
-            ->add('auteur', TextType::class, array(
-                'constraints' => array(new Assert\NotBlank(), 
-                    new Assert\Length(array('min' => 2)),
-                    new Assert\Length(array('max' => 30))
-                ),
-                'attr' => $default_attrs        
-            ))
-            ->add('editeur', TextType::class, array(
-                'constraints' => array(new Assert\NotBlank(), 
-                    new Assert\Length(array('min' => 2)),
-                    new Assert\Length(array('max' => 30))
-                ),
-                'attr' => $default_attrs       
-            ));
-            // ajout de la classe CSS js-datepicker pour le champ suivant 
-            $field_attrs = $default_attrs;
-            $field_attrs['class'] .= ' js-datepicker';
-            $this->form
-            ->add('parution', DateType::class, array(
-                'constraints' => array(new Assert\NotBlank()),
-                'widget' => 'single_text',
-
-                // do not render as type="date", to avoid HTML5 date pickers
-                'html5' => false,
-
-                // add a class that can be selected in JavaScript
-                'attr' => $field_attrs,
             ))
             ;            
         } else {
@@ -133,73 +83,136 @@ class AlbumbdForm {
             ->add('crud', HiddenType::class, array(
             ))
             ->add('id', HiddenType::class, array(
-            ))
-            ->add('album', TextType::class, array(
-                'attr' => $default_attrs       
-            ))     
-            ->add('auteur', TextType::class, array(
-                'attr' => $default_attrs        
-            ))
-            ->add('editeur', TextType::class, array(
-                'attr' => $default_attrs       
-            ))
-            ->add('parution', TextType::class, array(
-                'attr' => $default_attrs
-            ))
+            ))  
             ;
         }
+            
+        $this->genFields();
+        $this->genButtons();
 
+    }
+    
+    /**
+     * Définit les attributs HTML à appliquer par défaut à l'ensemble
+     * des champs du formulaire (selon le contexte d'exécution) 
+     * @throws \Exception
+     */
+    protected function setAttributes() {
+        // Ajout d’attributs HTML complémentaires selon le contexte
+        switch ($this->crud_context) {
+            case 'C': {
+                break;
+            }
+            case 'R': {
+                $this->default_attrs['disabled'] = 'disabled';
+                break;
+            }
+            case 'U': {
+                break;
+            }
+            case 'D': {
+                $this->default_attrs['disabled'] = 'disabled';
+                break;
+            }
+            default: {
+                throw new \Exception('Le parametre crud_context est incorrect') ;
+            }
+        }
+    }
+    
+    /**
+     * Génération des champs du formulaire
+     * Cette méthode doit impérativement être "overridée" dans les classes
+     * filles
+     */
+    protected function genFields() {
+        if ($this->crud_context == 'U' || $this->crud_context == 'C') {
+            $this->form        
+            // champ exemple
+            ->add('xxxxx', TextType::class, array(
+                'constraints' => array(new Assert\NotBlank(), 
+                    new Assert\Length(array('min' => 2)),
+                    new Assert\Length(array('max' => 30))
+                ),
+                'attr' => $this->default_attrs       
+            ))
+            ;            
+        } else {
+            // Si Consultation ou Suppression, affichage
+            // des champs verrouillés et sans contrôle
+            $this->form     
+            // champ exemple
+            ->add('xxxxx', TextType::class, array(
+                'attr' => $this->default_attrs       
+            ))     
+            ;
+        }        
+    }
+
+    /**
+     * Génération des boutons du formulaire
+     */
+    protected function genButtons() {
         // Définition des boutons de validation selon le contexte
         switch ($this->crud_context) {
             case 'C': {
                 $this->form
                     ->add('save', SubmitType::class, array(
-                        'attr' => array('label' => 'Enregistrer', 'class'=>'btn btn-success'),
+                        'attr' => array('label' => 'Enregistrer', 
+                                        'class'=>'btn btn-success'),
                     ))
                     ->add('reset', ResetType::class, array(
-                        'attr' => array('label' => 'Effacer', 'class'=>'btn btn-default'),
+                        'attr' => array('label' => 'Effacer', 
+                                        'class'=>'btn btn-default'),
                     ))
                     ->add('return', SubmitType::class, array(
-                        'attr' => array('label' => 'Annuler', 'class'=>'btn btn-default'),
+                        'attr' => array('label' => 'Annuler', 
+                                        'class'=>'btn btn-default'),
                     ));                        
                 break;
             }
             case 'R': {
                 $this->form
                     ->add('return', SubmitType::class, array(
-                        'attr' => array('label' => 'Retour', 'class'=>'btn btn-default'),
+                        'attr' => array('label' => 'Retour', 
+                                        'class'=>'btn btn-default'),
                     ));
                 break;
             }
             case 'U': {
                 $this->form
                     ->add('save', SubmitType::class, array(
-                        'attr' => array('label' => 'Enregistrer', 'class'=>'btn btn-success'),
+                        'attr' => array('label' => 'Enregistrer', 
+                                        'class'=>'btn btn-success'),
                     ))
                     ->add('reset', ResetType::class, array(
-                        'attr' => array('label' => 'Effacer', 'class'=>'btn btn-default'),
+                        'attr' => array('label' => 'Effacer', 
+                                        'class'=>'btn btn-default'),
                     ))
                     ->add('return', SubmitType::class, array(
-                        'attr' => array('label' => 'Annuler', 'class'=>'btn btn-default'),
+                        'attr' => array('label' => 'Annuler', 
+                                        'class'=>'btn btn-default'),
                     ));                        
                 break;
             }
             case 'D': {
                 $this->form
                     ->add('save', SubmitType::class, array(
-                        'attr' => array('label' => 'Confirmer', 'class'=>'btn btn-warning'),
+                        'attr' => array('label' => 'Confirmer', 
+                                        'class'=>'btn btn-warning'),
                     ))
                     ->add('return', SubmitType::class, array(
-                        'attr' => array('label' => 'Annuler', 'class'=>'btn btn-default'),
+                        'attr' => array('label' => 'Annuler', 
+                                        'class'=>'btn btn-default'),
                     ));
                 break;
             }
-        };        
+        };                
     }
     
     /**
-     * Utilise la fonction getForm() de l'objet $this->form
-     * @return type
+     * Renvoie la fonction getForm() de l'objet $this->form
+     * @return object
      */
     public function getForm() {
         return $this->form->getForm() ;
@@ -207,15 +220,15 @@ class AlbumbdForm {
     
     /**
      * Transmission de l'ID courant si besoin
-     * @return type
+     * @return integer
      */
     public function getId() {
         return $this->crud_id;
     }
     
     /**
-     * Transmission du contexte courant si besoin
-     * @return type
+     * Transmission du contexte courant ("C", "R", "U" ou "D")
+     * @return string
      */
     public function getContext() {
         return $this->crud_context ;
