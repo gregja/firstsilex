8e10856da65b202ad0f7d6b37b493f4db8e64f9a
Gregory Jarrige <gregory_jarrige@yahoo.fr>
2016-09-25 02:19:58 +0200

    table persons.sql

---
+++ archive/persons.sql
@@ -0,0 +1,112 @@
+
+CREATE TABLE persons (
+  id int(6) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  first_name varchar(30) NOT NULL DEFAULT '',
+  last_name varchar(30) NOT NULL DEFAULT '',
+  email varchar(60) NOT NULL DEFAULT '',
+  gender char(1) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+insert into persons (id, first_name, last_name, email, gender) values (1, 'Deborah', 'Thompson', 'dthompson0@csmonitor.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (2, 'Edward', 'Anderson', 'eanderson1@devhub.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (3, 'Philip', 'Alexander', 'palexander2@imdb.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (4, 'Juan', 'Crawford', 'jcrawford3@harvard.edu', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (5, 'Wayne', 'Cunningham', 'wcunningham4@liveinternet.ru', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (6, 'Gerald', 'Oliver', 'goliver5@ftc.gov', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (7, 'Benjamin', 'Patterson', 'bpatterson6@weebly.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (8, 'Brian', 'Clark', 'bclark7@hao123.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (9, 'Jimmy', 'Russell', 'jrussell8@aboutads.info', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (10, 'Aaron', 'Porter', 'aporter9@4shared.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (11, 'Jane', 'Austin', 'jaustina@fda.gov', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (12, 'Matthew', 'Morris', 'mmorrisb@photobucket.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (13, 'Emily', 'Hill', 'ehillc@linkedin.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (14, 'James', 'Ramirez', 'jramirezd@canalblog.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (15, 'Jessica', 'Gilbert', 'jgilberte@biblegateway.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (16, 'Angela', 'Edwards', 'aedwardsf@prlog.org', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (17, 'Patricia', 'Simpson', 'psimpsong@accuweather.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (18, 'Jonathan', 'Simmons', 'jsimmonsh@acquirethisname.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (19, 'Wanda', 'Montgomery', 'wmontgomeryi@smh.com.au', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (20, 'Jason', 'Kelley', 'jkelleyj@bloglines.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (21, 'Harold', 'Reid', 'hreidk@sun.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (22, 'Joshua', 'Fowler', 'jfowlerl@state.tx.us', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (23, 'Jeffrey', 'Chapman', 'jchapmanm@merriam-webster.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (24, 'Justin', 'Turner', 'jturnern@reuters.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (25, 'Arthur', 'Perez', 'aperezo@google.fr', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (26, 'Janet', 'Bradley', 'jbradleyp@livejournal.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (27, 'Paula', 'Williamson', 'pwilliamsonq@ycombinator.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (28, 'Sean', 'Kennedy', 'skennedyr@sina.com.cn', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (29, 'Paul', 'Stone', 'pstones@so-net.ne.jp', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (30, 'Bruce', 'Nguyen', 'bnguyent@bizjournals.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (31, 'Evelyn', 'Martinez', 'emartinezu@columbia.edu', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (32, 'John', 'Russell', 'jrussellv@163.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (33, 'Rose', 'Dunn', 'rdunnw@geocities.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (34, 'Sandra', 'Mitchell', 'smitchellx@opera.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (35, 'Christine', 'Johnson', 'cjohnsony@netlog.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (36, 'Edward', 'Thompson', 'ethompsonz@google.com.hk', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (37, 'Gary', 'Ramos', 'gramos10@miibeian.gov.cn', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (38, 'Sandra', 'Reyes', 'sreyes11@wikispaces.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (39, 'Diane', 'Carpenter', 'dcarpenter12@nifty.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (40, 'Brandon', 'Ramirez', 'bramirez13@yolasite.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (41, 'Virginia', 'Burke', 'vburke14@sakura.ne.jp', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (42, 'Clarence', 'Palmer', 'cpalmer15@vk.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (43, 'Patricia', 'Cruz', 'pcruz16@time.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (44, 'Diane', 'West', 'dwest17@dagondesign.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (45, 'Michael', 'Weaver', 'mweaver18@cam.ac.uk', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (46, 'Howard', 'Ford', 'hford19@51.la', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (47, 'Margaret', 'Perry', 'mperry1a@java.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (48, 'Lillian', 'Anderson', 'landerson1b@sourceforge.net', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (49, 'Christina', 'Sullivan', 'csullivan1c@ask.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (50, 'Fred', 'Flores', 'fflores1d@altervista.org', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (51, 'Todd', 'Perkins', 'tperkins1e@unblog.fr', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (52, 'Barbara', 'Chavez', 'bchavez1f@amazon.co.uk', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (53, 'Victor', 'Russell', 'vrussell1g@cdc.gov', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (54, 'Evelyn', 'Robinson', 'erobinson1h@wunderground.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (55, 'Jerry', 'Ross', 'jross1i@wikipedia.org', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (56, 'Todd', 'Fernandez', 'tfernandez1j@vkontakte.ru', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (57, 'William', 'Grant', 'wgrant1k@nba.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (58, 'Nicole', 'Dixon', 'ndixon1l@army.mil', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (59, 'Christopher', 'Welch', 'cwelch1m@behance.net', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (60, 'Linda', 'Kim', 'lkim1n@webmd.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (61, 'Marie', 'Flores', 'mflores1o@wsj.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (62, 'Judith', 'Walker', 'jwalker1p@list-manage.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (63, 'Nicholas', 'Fernandez', 'nfernandez1q@disqus.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (64, 'Aaron', 'Lewis', 'alewis1r@reverbnation.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (65, 'Clarence', 'Romero', 'cromero1s@paypal.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (66, 'Johnny', 'Carter', 'jcarter1t@globo.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (67, 'Jose', 'Lawrence', 'jlawrence1u@hud.gov', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (68, 'Randy', 'Dunn', 'rdunn1v@livejournal.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (69, 'Joseph', 'Sanchez', 'jsanchez1w@phpbb.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (70, 'Sarah', 'Ryan', 'sryan1x@webmd.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (71, 'Martha', 'Cunningham', 'mcunningham1y@phoca.cz', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (72, 'Bruce', 'Henry', 'bhenry1z@360.cn', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (73, 'Jason', 'Garza', 'jgarza20@omniture.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (74, 'Thomas', 'Johnston', 'tjohnston21@oakley.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (75, 'Carol', 'Grant', 'cgrant22@sciencedirect.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (76, 'Ralph', 'Bradley', 'rbradley23@go.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (77, 'Susan', 'Perry', 'sperry24@loc.gov', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (78, 'Charles', 'Boyd', 'cboyd25@simplemachines.org', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (79, 'Todd', 'Hamilton', 'thamilton26@ox.ac.uk', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (80, 'Clarence', 'Gray', 'cgray27@ow.ly', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (81, 'Judith', 'Hart', 'jhart28@dmoz.org', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (82, 'Donald', 'Young', 'dyoung29@nba.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (83, 'Brian', 'Olson', 'bolson2a@canalblog.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (84, 'Evelyn', 'Price', 'eprice2b@usgs.gov', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (85, 'Kathleen', 'Rogers', 'krogers2c@is.gd', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (86, 'Jonathan', 'Cruz', 'jcruz2d@yahoo.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (87, 'Scott', 'Robertson', 'srobertson2e@abc.net.au', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (88, 'Sara', 'Cooper', 'scooper2f@netvibes.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (89, 'Joan', 'Duncan', 'jduncan2g@oakley.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (90, 'Harry', 'Hernandez', 'hhernandez2h@blinklist.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (91, 'Michelle', 'Allen', 'mallen2i@uiuc.edu', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (92, 'Chris', 'Ray', 'cray2j@ed.gov', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (93, 'Rose', 'Kennedy', 'rkennedy2k@smugmug.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (94, 'Edward', 'Patterson', 'epatterson2l@fastcompany.com', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (95, 'Steve', 'Bennett', 'sbennett2m@usa.gov', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (96, 'Kimberly', 'Rice', 'krice2n@paypal.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (97, 'Annie', 'Russell', 'arussell2o@ustream.tv', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (98, 'Janice', 'Wallace', 'jwallace2p@stumbleupon.com', 'F');
+insert into persons (id, first_name, last_name, email, gender) values (99, 'Martin', 'Thompson', 'mthompson2q@t-online.de', 'M');
+insert into persons (id, first_name, last_name, email, gender) values (100, 'David', 'Russell', 'drussell2r@ucoz.ru', 'M');
+
+
